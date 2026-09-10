c561b29ee25917b1e1b6790e57691cd1605c9631
Liam Ryan <liamryan@gmail.com>
2026-09-09 22:46:06 -0400

    hamburger

--- js/header.js
+++ js/header.js
@@ -2,4 +2,19 @@ fetch("header.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("header-placeholder").innerHTML = data;
+
+        const hamburger = document.getElementById("hamburger");
+        const navigation = document.getElementById("navigation");
+
+        hamburger.addEventListener("click", function() {
+            navigation.classList.toggle("open");
+        });
+
+        const smallScreen = window.matchMedia("(max-width: 900px)");
+
+        smallScreen.addEventListener("change", function(event) {
+            if (!event.matches) {
+                navigation.classList.remove("open");
+            }
+        });
     });
