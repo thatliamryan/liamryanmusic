2a392bbef44aef8192042cc5076b14f81c5f8c75
Liam Ryan <liamryan@gmail.com>
2026-09-13 14:38:19 -0400

    Money money

--- js/header.js
+++ js/header.js
@@ -1,20 +1,25 @@
 fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("header-placeholder").innerHTML = data;
+.then(response => response.text())
+.then(data => {
+    document.getElementById("header-placeholder").innerHTML = data;
 
-        const hamburger = document.getElementById("hamburger");
-        const navigation = document.getElementById("navigation");
+    const hamburger = document.getElementById("hamburger");
+    const navigation = document.getElementById("navigation");
+    const closeMenu = document.getElementById("close-menu");
 
-        hamburger.addEventListener("click", function() {
-            navigation.classList.toggle("open");
-        });
-
-        const smallScreen = window.matchMedia("(max-width: 900px)");
-
-        smallScreen.addEventListener("change", function(event) {
-            if (!event.matches) {
-                navigation.classList.remove("open");
-            }
-        });
+    hamburger.addEventListener("click", function() {
+        navigation.classList.toggle("open");
     });
+
+    closeMenu.addEventListener("click", function() {
+        navigation.classList.remove("open");
+    });
+
+    const smallScreen = window.matchMedia("(max-width: 1100px)");
+
+    smallScreen.addEventListener("change", function(event) {
+        if (!event.matches) {
+            navigation.classList.remove("open");
+        }
+    });
+});
